ad2ff776e8cf39c7313bab7c6519d1c29246a619
github username <github email address>
2020-02-27 14:31:59 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO "bears" ("id", "name", "age", "gender",
+"color", "temperament", "alive") VALUES 
+(1, "Mr. Chocolate", 14, "M", "Brown", "Nice", 0);
+INSERT INTO "bears" ("id", "name", "age", "gender",
+"color", "temperament", "alive") VALUES 
+(2, "Rowdy", 7, "F", "Black", "Sassy", 1);
+INSERT INTO "bears" ("id", "name", "age", "gender",
+"color", "temperament", "alive") VALUES 
+(3, "Tabitha", 200, "F", "Unknown", "Always Watching", 1);
+INSERT INTO "bears" ("id", "name", "age", "gender",
+"color", "temperament", "alive") VALUES 
+(4, "Sergeant Brown", 21, "M", "Brown", "Strict", 1);
+INSERT INTO "bears" ("id", "name", "age", "gender",
+"color", "temperament", "alive") VALUES 
+(5, "Melissa", 14, "F", "Brown", "Nice", 1);
+INSERT INTO "bears" ("id", "name", "age", "gender",
+"color", "temperament", "alive") VALUES 
+(6, "Grinch", 3, "M", "Brown", "Grumpy", 1);
+INSERT INTO "bears" ("id", "name", "age", "gender",
+"color", "temperament", "alive") VALUES 
+(7, "Wendy", 21, "F", "Grey", "Nice", 1);
+INSERT INTO "bears" ("id", "name", "age", "gender",
+"color", "temperament", "alive") VALUES 
+(8, NULL, 30, "M", "Unknown", "Unknown", 1);
